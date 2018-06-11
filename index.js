9579f291a7bb717d81a1ae33d9f3af3c4ebedf44
nastia <mixartemev@gmail.com>
2018-06-11 15:06:14 +0300

    clean

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const http = require('http');
+//global arr of visit timestamps
 let cnt = [];
 
 http.createServer((req, res) => {
     switch (req.url){
         case '/':
+            //del all visits older 1min
             cnt = cnt.filter(visitTime => visitTime > Date.now() - 60*1000);
+            //push current visit timestamp
             cnt.push(Date.now());
             res.end('views/min: ' + cnt.length);
-            console.log(cnt);
-            break;
-
-        default:
-            res.statusCode = 404;
-            res.end('Not found');
     }
 }).listen(3000);
