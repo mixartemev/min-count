b53bfc88877938993c6325e77232e8863a1790c2
nastia <mixartemev@gmail.com>
2018-06-12 15:15:48 +0300

    minify

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,12 @@ FROM node:alpine
 # Create app directory
 WORKDIR /usr/src/app
 
+#we dont need any dependencies in prod
 #COPY package*.json ./
 #RUN npm install
 
 # Bundle app source
-COPY . .
+COPY public ./
 
 # tell the port number the container should expose
 EXPOSE 3000
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const [http, fresh] = [
         return () => {
             let i = 0;
             //find first index, whos timestamp older 1min
-            while (visits[i] < Date.now() - 10*1000) { i++ }
+            while (visits[i] < Date.now() - 60*1000) { i++ }
             visits.splice(0, i);
             visits.push(Date.now());
             return visits.length;
